c7ed41721330ea37fc50a440dc2bee60af2bde3b
Andrew Herchykov <andriiwp3@gmail.com>
2024-07-25 21:52:45 +0100

    Base implementation of api client

--- src/apiClient.js
+++ src/apiClient.js
@@ -1,13 +1,57 @@
 class ApiClient {
-    get() { }
+    #baseUrl;
+
+    async get() {
+        const response = await fetch(this.#generateUrl(url), {
+            method: 'GET',
+        });
+
+        return response.json();
+    }
     
-    post() { }
+    async post(url, { body, headers }) {
+        const response = await fetch(this.#generateUrl(url), {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers
+        });
 
-    put() { }
+        return response.json();
+    }
 
-    patch() { }
+    async put(url, { body, headers }) {
+        const response = await fetch(this.#generateUrl(url), {
+            method: 'PUT',
+            body: JSON.stringify(body),
+            headers
+        });
 
-    delete() { }
+        return response.json();
+    }
+
+    async patch(url, { body, headers }) {
+        const response = await fetch(this.#generateUrl(url), {
+            method: 'PATCH',
+            body: JSON.stringify(body),
+            headers
+        });
+
+        return response.json();
+    }
+
+    async delete(url, { body, headers }) {
+        const response = await fetch(this.#generateUrl(url), {
+            method: 'DELETE',
+            body: JSON.stringify(body),
+            headers
+        });
+
+        return response.json();
+    }
+
+    #generateUrl(path) {
+        return this.#baseUrl + path;
+    }
 }
 
 module.exports = ApiClient;
