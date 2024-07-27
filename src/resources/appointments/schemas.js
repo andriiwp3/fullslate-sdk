f3e5090aafabd545643fb2f543870bf77da51347
Andrew Herchykov <andriiwp3@gmail.com>
2024-07-27 23:56:12 +0100

    Improve appointments validation schemas

--- src/resources/appointments/schemas.js
+++ src/resources/appointments/schemas.js
@@ -1,33 +1,107 @@
 const { z } = require("zod");
 
-const id = z.string().min(1);
+const service = z.number().positive();
+const employee = z.number().positive();
 
-const getScheduled = z.object({
+const appointment = z.object({
+    id: z.string().min(1),
+    at: z.date(),
+    to: z.date(),
+    services: z.array(service),
+    employee,
+    location_id: z.number().positive(),
+    custom_fields: z.array(z.object({ label: z.string(), value: z.string() })),
+    client_with_creation: z.object({
+        first_name: z.string().min(1),
+        last_name: z.string().min(1),
+        birthday: z.date(),
+        no_automatic_email: z.boolean(),
+        no_sms: z.boolean(),
+        mass_email_opt_in: z.boolean(),
+        sms_reminder_consent: z.boolean(),
+        phone_number: z.object({
+            number: z.string().min(1),
+            contact_type: z.enum(['HOME', 'MOBILE', 'WORK', 'FAX', 'PAGER', 'OTHER', 'PRIMARY', 'SECONDARY'])
+        }),
+        address: z.object({
+            street1: z.string().min(1),
+            street2: z.string(),
+            city: z.string(),
+            state: z.string(),
+            postal_code: z.string(),
+        }),
+        email: z.string(),
+        time_zone: z.string(),
+    }),
+    client: z.number().positive(),
+    recurrence_mode: z.string(),
+    recurrence_interval: z.number(),
+    recur_end_at: z.date(),
+    notes: z.string(),
+    client_notes: z.string(),
+    promo_code: z.string(),
+    status: z.enum(['STATUS_NO_SHOW', 'STATUS_CHECKED_IN', 'STATUS_COMPLETE', 'STATUS_BOOKED']),
+    api_options: z.object(z.any()),
+    client_preferred_employee: z.boolean(),
+    confirmed: z.boolean(),
+    send_client_confirmation_email: z.boolean(),
+    send_employee_notification_email: z.boolean(),
+    user_type: z.enum(['CLIENT', 'BUSINESS_USER']),
+    passphrase: z.string(),
 })
 
-const create = z.object({
-})
+const create = appointment.omit({ id: true });
 
-const get = z.object({
-    id,
+const getReview = appointment.pick({ employees: true })
+
+const getScheduled = appointment.pick({
+    to: true,
+    status: true,
+    confirmed: true
+}).extend({
+    from: z.date(),
+    employees: z.array(employee),
+    service,
+    include_deleted,
+    changed_since,
+    client_id,
+});
+
+const get = appointment.pick({ id: true }).extend({
     state_at: z.date(),
+});
+
+const update = appointment.omit({
+    location_id: true,
+    recurrence_mode: true,
+    recur_end_at: true,
+    promo_code: true,
+    client_preferred_employee: true,
+    passphrase: true,
+}).extend({
+    edit_at: z.date(),
+    update_recurrence_flag: z.enum(['FUTURE_OCCURRENCE', 'SINGLE_OCCURRENCE']),
+    reassign_employee_if_necessary: z.boolean(),
+});
+
+const remove = appointment.pick({
+    id: true,
+    at: true,
+    send_employee_notification_email: true,
+    user_type: true,
+}).extend({
+    delete_future_occurences: z.boolean(),
+    send_client_cancel_email: z.boolean(),
+    cancellation_message: z.string()
 })
 
-const update = z.object({
-    id,
+const accept = appointment.pick({ id: true }).extend({
+    notify_client: z.boolean(),
+    accept_with_conflict: z.boolean(),
 })
 
-const remove = z.object({
-    id,
-})
-
-const accept = z.object({
-})
-
-const decline = z.object({
-})
-
-const getReview = z.object({
+const decline = appointment.pick({ id: true }).extend({
+    notify_client: z.boolean(),
 })
 
 module.exports = {
