e60621c3a535d9c31c257d5258033f46ccce0992
Andrew Herchykov <andriiwp3@gmail.com>
2024-07-28 13:57:17 +0100

    Setup tests

---
+++ tests/mocks/apiClient.mock.js
@@ -0,0 +1,10 @@
+import { vi } from 'vitest';
+
+export default Object.freeze({
+    get: vi.fn(),
+    post: vi.fn(),
+    patch: vi.fn(),
+    put: vi.fn(),
+    delete: vi.fn(),
+    get: vi.fn(),
+});
